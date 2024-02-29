ed7a4d87cb9c7909472fbac6f8a785ffa56fe842
iknowca <iknowca@gmail.com>
2024-02-29 19:50:26 +0900

    feat: AppBar 구현 [SSF-6]

---
+++ src/components/header/HeaderBar.vue
@@ -0,0 +1,15 @@
+<template lang="">
+<v-app-bar class="d-flex flex-row justify-center">
+    <v-img @click="router.push('/')" class="mx-2" src="@/assets/logo.png"></v-img>
+    <v-spacer></v-spacer>
+    <v-btn @click="router.push('/account/login')">들어가기</v-btn>
+    <v-btn @click="router.push('/account/join')">가입하기</v-btn>
+</v-app-bar>
+</template>
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+<style lang="">
+    
+</style>
