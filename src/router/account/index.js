316a44576d775a1269d5df432c7054478f622f03
iknowca <iknowca@gmail.com>
2024-03-05 23:20:58 +0900

    feat: 유저 비밀번호 변경 페이지 추가 [SSF-23]

--- src/router/account/index.js
+++ src/router/account/index.js
@@ -2,6 +2,7 @@ import JoinView from '@/views/account/JoinView'
 import OauthCallbackView from '@/views/account/oauth/OauthCallbackView'
 import MyPageView from '@/views/account/MyPageView'
 import LoginView from "@/views/account/LoginView.vue";
+import PasswordUpdateView from "@/views/account/PasswordUpdateView.vue";
 
 const accountRouter = [
     {
@@ -23,6 +24,11 @@ const accountRouter = [
         path: '/account/login',
         name: 'login',
         component: LoginView
+    },
+    {
+        path: '/account/mypage/password',
+        name: 'password',
+        component: PasswordUpdateView
     }
 ]
 
